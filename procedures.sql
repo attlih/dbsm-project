ec64da2026919e47efbff6e7e6c9cbfe10cdf30b
AVH <80627322+AntonHelminen@users.noreply.github.com>
2023-04-29 15:08:28 +0300

    Procedures
    All procedures complete.

---
+++ procedures.sql
@@ -0,0 +1,41 @@
+-- Creates a procedure to set salary to base level based on job title
+CREATE OR REPLACE PROCEDURE set_salary_based_on_title()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	UPDATE employee
+    SET salary = job_title.base_salary
+	FROM job_title
+    WHERE employee.j_id = job_title.j_id;
+END
+$$;
+
+-- Creates a procedure to add 3 months to temporary contracts
+CREATE OR REPLACE PROCEDURE add_3_months_to_contracts()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	UPDATE employee
+    SET contract_end = contract_end + INTERVAL '3 MONTHS'
+    WHERE contract_type = 'Temporary';
+END
+$$;
+
+-- Creates a procedure to increase salaries by a given percentage (decimal). A maximum limit can also be given
+-- for the salaries to be increased.
+CREATE OR REPLACE PROCEDURE increase_salary_by_percentage(percentage numeric, max_limit integer)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	IF (max_limit IS NOT NULL AND max_limit != 0) THEN
+		UPDATE employee
+    	SET salary = salary + salary * percentage
+    	WHERE salary <= max_limit;
+	END IF;
+	
+	IF (max_limit IS NULL OR max_limit = 0) THEN
+		UPDATE employee
+    	SET salary = salary + salary * percentage;
+	END IF;
+END
+$$;
