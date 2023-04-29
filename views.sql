f149b6b89807cc7102687825f02f484f898fe420
AVH <80627322+AntonHelminen@users.noreply.github.com>
2023-04-29 13:47:40 +0300

    Add files via upload

--- views.sql
+++ views.sql
@@ -1,1 +1,6 @@
---Creates views
+SELECT employee.e_id AS employee_id, employee.emp_name AS employee_name, STRING_AGG(DISTINCT skills.skill, ', ') AS skill
+FROM employee
+INNER JOIN employee_skills ON employee.e_id = employee_skills.e_id
+INNER JOIN skills ON skills.s_id = employee_skills.s_id
+GROUP BY employee_id, employee_name;
+
