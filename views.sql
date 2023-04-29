bc86327076a925f324aec1d70f0cf909949385df
attlih <98050742+attlih@users.noreply.github.com>
2023-04-29 14:16:11 +0300

    fix join method

--- views.sql
+++ views.sql
@@ -11,7 +11,8 @@ INNER JOIN geo_location ON geo_location.l_id = headquarters.l_id;
 
 -- create view to show employees department, headquarters and location
 CREATE OR REPLACE VIEW employees_in_department AS
-SELECT e_id, emp_name, dep_name, hq_name, street, city, country, geo_location.l_id FROM employee, department, headquarters, geo_location
-WHERE employee.d_id = department.d_id
-AND department.hid = headquarters.h_id
-AND headquarters.l_id = geo_location.l_id;
+SELECT e_id, emp_name, dep_name, hq_name, geo_location.l_id, street, city, country FROM employee
+LEFT JOIN department ON employee.d_id = department.d_id
+LEFT JOIN headquarters ON department.hid = headquarters.h_id
+LEFT JOIN geo_location ON headquarters.l_id = geo_location.l_id;
+
