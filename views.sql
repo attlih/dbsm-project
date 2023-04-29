62c846cdc7e3866eaf7f5376f67baf44d4c8b6a2
attlih <98050742+attlih@users.noreply.github.com>
2023-04-29 14:09:17 +0300

    add view

--- views.sql
+++ views.sql
@@ -8,3 +8,10 @@ GROUP BY employee_id, employee_name;
 -- All headquarters and their locations
 SELECT h_id, hq_name, street, city, country FROM headquarters
 INNER JOIN geo_location ON geo_location.l_id = headquarters.l_id;
+
+-- create view to show employees department, headquarters and location
+CREATE OR REPLACE VIEW employees_in_department AS
+SELECT e_id, emp_name, dep_name, hq_name, street, city, country, geo_location.l_id FROM employee, department, headquarters, geo_location
+WHERE employee.d_id = department.d_id
+AND department.hid = headquarters.h_id
+AND headquarters.l_id = geo_location.l_id;
