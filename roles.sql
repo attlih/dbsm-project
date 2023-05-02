5eb4a58aa2c3083b97e06b9017252bbc79a41e97
attlih <98050742+attlih@users.noreply.github.com>
2023-05-02 16:51:57 +0300

    add role views only

--- roles.sql
+++ roles.sql
@@ -5,13 +5,28 @@
 CREATE ROLE admin;
 CREATE ROLE employee;
 CREATE ROLE trainee;
+CREATE ROLE views_only;
 
 -- Give admin all administrative rights (same rights as postgres superuser would have)
 GRANT ALL ON ALL TABLES IN SCHEMA public TO admin;
 -- Give employee rights to read all information in the database but no rights to write
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO employee;
 -- Give trainee rights to read ONLY project, customer, geo_location, and project_role tables as well as limited access to employee table (only allow reading employee id, name, email)
-GRANT SELECT ON project, customer, geo_location, project_role TO trainee;
+GRANT SELECT ON
+  project,
+  customer,
+  geo_location,
+  project_role
+TO trainee;
+-- An additional role called views_only and give them read access to all created views (and nothing else)
+GRANT SELECT ON
+  employees_and_skills,
+  headquarter_locations,
+  projects_and_customers,
+  employees_in_department,
+  all_supervisors,
+  people_in_department
+TO views_only;
 
 
 
